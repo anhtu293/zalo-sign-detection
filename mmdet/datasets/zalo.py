446d56e91fbf2d1e7309d5e4bde7de876f0bec7c
anhtu <anhtu293@gmail.com>
2020-11-05 21:54:29 +0100

    fix unicode bug in dataset

--- mmdet/datasets/zalo.py
+++ mmdet/datasets/zalo.py
@@ -5,6 +5,6 @@ from .builder import DATASETS
 @DATASETS.register_module()
 class ZaloDataset(CocoDataset):
 
-    CLASSES = ('Cấm ngược chiều', 'Cấm dừng và đỗ', 'Cấm rẽ',
-               'Giới hạn tốc độ', 'Cấm còn lại', 'Nguy hiểm',
-               'Hiệu lệnh')
+    CLASSES = ('Cam nguoc chieu', 'Cam dung', 'Cam re',
+               'Gioi han toc do', 'Cam con lai', 'Nguy hiem',
+               'Hieu lenh')
