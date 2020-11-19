2d0bf37fa90fd40fd4ec3f3221961f1ef715c8ea
anhtu <anhtu293@gmail.com>
2020-11-19 19:50:27 +0100

    train retina

--- configs/zalo_challenge/ssd.py
+++ configs/zalo_challenge/ssd.py
@@ -1,76 +1,78 @@
 # model settings
+input_size = 300
 model = dict(
-    type='RetinaNet',
-    # pretrained='torchvision://resnet50',
+    type='SingleStageDetector',
+    pretrained='open-mmlab://vgg16_caffe',
     backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch'),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        start_level=1,
-        add_extra_convs='on_input',
-        num_outs=5),
+        type='SSDVGG',
+        input_size=input_size,
+        depth=16,
+        with_last_pool=False,
+        ceil_mode=True,
+        out_indices=(3, 4),
+        out_feature_indices=(22, 34),
+        l2_norm_scale=20),
+    neck=None,
     bbox_head=dict(
-        type='RetinaHead',
-        num_classes=1,
-        in_channels=256,
-        stacked_convs=4,
-        feat_channels=256,
+        type='SSDHead',
+        in_channels=(512, 1024, 512, 256, 256, 256),
+        num_classes=80,
         anchor_generator=dict(
-            type='AnchorGenerator',
-            octave_base_scale=4,
-            scales_per_octave=3,
-            ratios=[0.8, 1.0, 1.2],
-            strides=[8, 16, 32, 64, 128]),
+            type='SSDAnchorGenerator',
+            scale_major=False,
+            input_size=input_size,
+            basesize_ratio_range=(0.15, 0.9),
+            strides=[8, 16, 32, 64, 100, 300],
+            ratios=[[1], [2, 1]]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0]),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)))
-# training and testing settings
+            target_stds=[0.1, 0.1, 0.2, 0.2])))
+cudnn_benchmark = True
 train_cfg = dict(
     assigner=dict(
         type='MaxIoUAssigner',
         pos_iou_thr=0.5,
-        neg_iou_thr=0.4,
-        min_pos_iou=0,
-        ignore_iof_thr=-1),
+        neg_iou_thr=0.5,
+        min_pos_iou=0.,
+        ignore_iof_thr=-1,
+        gt_max_assign_all=False),
+    smoothl1_beta=1.,
     allowed_border=-1,
     pos_weight=-1,
+    neg_pos_ratio=3,
     debug=False)
 test_cfg = dict(
-    nms_pre=1000,
+    nms=dict(type='nms', iou_threshold=0.45),
     min_bbox_size=0,
-    score_thr=0.3,
-    nms=dict(type='nms', iou_threshold=0.3),
-    max_per_img=100)
+    score_thr=0.02,
+    max_per_img=200)
+
 
 dataset_type = 'ZaloDataset'
 data_root = '/workspace/zalo_sign_detection/za_traffic_2020/traffic_train/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(800, 400), (1311,313)], keep_ratio=True),
-    dict(type='PhotoMetricDistortion'),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(300, 300), keep_ratio=False),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
@@ -78,13 +80,11 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1622, 626),
+        img_scale=(300, 300),
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
+            dict(type='Resize', keep_ratio=False),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
@@ -110,7 +110,7 @@ data = dict(
 evaluation = dict(interval=1, metric='bbox')
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9,  weight_decay=0.0001) # 
+optimizer = dict(type='SGD', lr=2e-3, momentum=0.9, weight_decay=5e-4)
 # batch_size/800
 optimizer_config = dict(grad_clip=None)
 # learning policy
@@ -119,9 +119,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    # step=[26, 29]
-    step=[26, 33]
-    )
+    step=[26, 29])
 total_epochs = 50
 
 checkpoint_config = dict(interval=1)
@@ -138,4 +136,3 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-
